416c052430b03d91a16affeca95df57a4a686d65
Ming424 <tianming.chen0424@gmail.com>
2022-07-10 20:43:40 -0400

    bootstrap + service

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,6 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class Vet extends Person{
+import java.util.Set;
+
+public class Vet extends Person {
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+
+    private Set<Speciality> specialities;
 
     @Override
     public String toString() {
